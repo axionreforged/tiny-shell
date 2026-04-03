2fe47f93b9a3fc8c2c5e3a6d63970c07c50fef53
axionreforged <aldin.adilovic97@gmail.com>
2026-04-03 23:26:43 +0200

    Implemented parser for single pipe

--- include/tokenize.h
+++ include/tokenize.h
@@ -1,8 +1,10 @@
 #ifndef TOKENIZE_H
 #define TOKENIZE_H
 
+
 void tokenize(const char* line, char *tokens[]);
 void print_tokens(char *tokens[]);
 void free_memory_of_tokens(char *tokens[]);
-
+enum pipe_status{NO_LEFT_COMMAND, NO_RIGHT_COMMAND, MORE_THAN_ONE_PIPE, PIPE_VALID, NO_PIPE};
+enum pipe_status parser(const char *line, char *tokens[], char *cmd1[], char *cmd2[]);
 #endif
--- src/tokenize.c
+++ src/tokenize.c
@@ -21,7 +21,7 @@ void tokenize(const char *line, char *tokens[]) {
             break;
         }
 
-        if (line[line_index] == '>' || line[line_index] == '<') {
+        if (line[line_index] == '>' || line[line_index] == '<' || line[line_index] == '|') {
             tokens[token_index] = malloc(2);
             if (tokens[token_index] == NULL) {
                 perror("malloc");
@@ -46,7 +46,8 @@ void tokenize(const char *line, char *tokens[]) {
         while (line[line_index] != '\0' &&
                line[line_index] != ' ' &&
                line[line_index] != '>' &&
-               line[line_index] != '<') {
+               line[line_index] != '<' &&
+               line[line_index] != '|') {
             if (word_index < 100) {
                 tokens[token_index][word_index] = line[line_index];
                 word_index++;
@@ -78,4 +79,51 @@ void free_memory_of_tokens(char *tokens[]) {
         tokens[index] = NULL;
         index++;
     }
+}
+
+int check_for_pipe(char *tokens[], int starting_index){
+    int index = starting_index;
+    while(tokens[index] != NULL){
+        if(strcmp(tokens[index], "|") == 0) return index;
+        index++;
+    }
+    return -1;
+}
+
+enum pipe_status validate_pipe(char *tokens[], int index_of_pipe){
+    if(index_of_pipe == 0) return NO_LEFT_COMMAND;
+    else if(tokens[index_of_pipe + 1] == NULL) return NO_RIGHT_COMMAND;
+    else if(check_for_pipe(tokens, index_of_pipe + 1) > -1) return MORE_THAN_ONE_PIPE;
+    else return PIPE_VALID;
+}
+
+void split_on_pipe(char *tokens[], int index_of_pipe, char *cmd1[], char *cmd2[]){
+    int index = 0;
+    while(index < index_of_pipe){
+        cmd1[index] = tokens[index];
+        index++;
+    }
+    cmd1[index] = NULL;
+    index++;
+
+    int cmd2_index = 0;
+    while(tokens[index] != NULL){
+        cmd2[cmd2_index] = tokens[index];
+        index++;
+        cmd2_index++;
+    }
+    cmd2[cmd2_index] = NULL;
+}
+
+enum pipe_status parser(const char *line, char *tokens[], char *cmd1[], char *cmd2[]){
+    tokenize(line, tokens);
+    int index_of_pipe = check_for_pipe(tokens, 0);
+    cmd1[0] = NULL;
+    cmd2[0] = NULL;
+    if(index_of_pipe > -1){
+        enum pipe_status validation = validate_pipe(tokens, index_of_pipe);
+        if(validation == PIPE_VALID) split_on_pipe(tokens, index_of_pipe, cmd1, cmd2);
+        return validation;
+    }
+    return NO_PIPE;
 }
